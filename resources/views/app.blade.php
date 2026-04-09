2651f5e4e3af60538253023e5a328c52f6a42abb
Kay van Aarssen <kayvanaarssen@MBP-van-Kay-2.kavaa.tools>
2026-04-09 11:03:04 +0200

    Remove old favicon.ico, fix favicon link tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'PasteBucket') }}</title>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" href="/favicon.svg">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700|jetbrains-mono:400,500" rel="stylesheet" />
         <script>
